b7f638c82153988ef4d1d96e92295d577c8b9fa9
Andrew Danise <ajdanise@gmail.com>
2017-03-27 02:33:02 -0400

    make sim read from test.bin again

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -22,31 +22,31 @@ main(int argc, char** argv) {
     //fwrite(program1.data(), sizeof(uint32_t), program1.size(), wf);
     //fclose(wf);
 
-    //FILE* file = fopen("test.bin", "rb");
+    FILE* file = fopen("test.bin", "rb");
 
-    //// get file size in instruction words
-    //fseek(file, 0L, SEEK_END);
-    //int sz = ftell(file);
-    //fseek(file, 0L, SEEK_SET);
+    // get file size in instruction words
+    fseek(file, 0L, SEEK_END);
+    int sz = ftell(file);
+    fseek(file, 0L, SEEK_SET);
 
-    //int sz_in_words = sz / sizeof(uint32_t);
+    int sz_in_words = sz / sizeof(uint32_t);
 
-    //// read that many words from the file
-    //vector<uint32_t> program2(sz_in_words);
-    //fread(program2.data(), sizeof(uint32_t), sz_in_words, file);
+    // read that many words from the file
+    vector<uint32_t> program2(sz_in_words);
+    fread(program2.data(), sizeof(uint32_t), sz_in_words, file);
 
-    //fclose(file);
+    fclose(file);
 
-    const vector<uint32_t> program2 = {
-        (uint32_t)(0b00 << 30 | 0b11100 << 25 | 0b00000 << 20 | 16),
-        (uint32_t)(0b00 << 30 | 0b11100 << 25 | 0b00001 << 20 | 100),
-        (uint32_t)(0b00 << 30 | 0b11100 << 25 | 0b00010 << 20 | 0),
-        (uint32_t)(0b00 << 30 | 0b00101 << 25 | 0b00000 << 20 | 0b00001 << 15),
-  //      (uint32_t)(0b10 << 30 | 0b00    << 28 | 0b111111111111111111111100),
-        (uint32_t)(0b10 << 30 | 0b01    << 28 | 0b0000 << 24 | 0b00010 << 19),
-        (uint32_t)(0b10 << 30 | 0b00    << 28 | 0b0010 << 24 | 4),
-        (uint32_t)(0b10 << 30 | 0b00    << 28 | 0b0000 << 24 | 0),
-    };
+//    const vector<uint32_t> program2 = {
+//        (uint32_t)(0b00 << 30 | 0b11100 << 25 | 0b00000 << 20 | 16),
+//        (uint32_t)(0b00 << 30 | 0b11100 << 25 | 0b00001 << 20 | 100),
+//        (uint32_t)(0b00 << 30 | 0b11100 << 25 | 0b00010 << 20 | 0),
+//        (uint32_t)(0b00 << 30 | 0b00101 << 25 | 0b00000 << 20 | 0b00001 << 15),
+//        (uint32_t)(0b10 << 30 | 0b00    << 28 | 0b111111111111111111111100),
+//  //      (uint32_t)(0b10 << 30 | 0b01    << 28 | 0b0000 << 24 | 0b00010 << 19),
+//        (uint32_t)(0b10 << 30 | 0b00    << 28 | 0b0010 << 24 | 4),
+//        (uint32_t)(0b10 << 30 | 0b00    << 28 | 0b0000 << 24 | 0),
+//    };
 
     CPU cpu(program2);
     int num_cycles = cpu.exec();
